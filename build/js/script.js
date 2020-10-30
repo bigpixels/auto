d64175fc767b34c023234f350fe250d5e5ebce0c
bigpixels <gamer21centuries@gmail.com>
2020-10-30 13:19:57 +0200

    humburger
    update navigation

---
+++ build/js/script.js
@@ -0,0 +1,36 @@
+'use strict';
+$(function () {
+    //new $(document).ready
+    let body = $('body');
+    let $hamburger = $('.hamburger');
+    let $overlay = $('.overlay');
+    let $phones = $('.phones');
+
+    function  window_size() {
+        if ( body.innerWidth() < 767) {
+            $phones.appendTo('.navigation');
+        }
+    }
+    $(window).on('load resize', window_size)
+
+    function toggleMobileMenu() {
+        $hamburger
+        .toggleClass('is-active')
+        .next()
+        .toggleClass('active');
+        $overlay.fadeToggle();
+        $('body, html').toggleClass('no-scroll');
+    }
+
+    (function showMobileMenu() {
+        $hamburger.on('click', function() {
+            toggleMobileMenu();
+        });
+    })();
+
+    (function HideMobileMenu() {
+        $overlay.on('click', function() {
+            toggleMobileMenu();
+        });
+    })();
+});
