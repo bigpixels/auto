32b722307cd3ea317fd3c93f8b9e0d55ed02faa5
bigpixels <gamer21centuries@gmail.com>
2020-11-03 00:34:40 +0200

    header phone to burger
    update header mobile

--- build/js/script.js
+++ build/js/script.js
@@ -7,8 +7,10 @@ $(function () {
     let $phones = $('.phones');
 
     function  window_size() {
-        if ( body.innerWidth() < 767) {
-            $phones.appendTo('.navigation');
+        if ( body.innerWidth() > 767) {
+            $phones.appendTo('.header__inner');
+        } else {
+            $phones.appendTo('.navigation')
         }
     }
     $(window).on('load resize', window_size)
@@ -18,6 +20,7 @@ $(function () {
         .toggleClass('is-active')
         .next()
         .toggleClass('active');
+        $phones.toggleClass('active');
         $overlay.fadeToggle();
         $('body, html').toggleClass('no-scroll');
     }
